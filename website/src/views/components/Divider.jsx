13c5883aa0a76541f6c72bf768e795d9976bb3ce
Byron Sellan <41913864+HellFire0325@users.noreply.github.com>
2024-06-24 13:33:38 -0500

    Delete website directory

--- website/src/views/components/Divider.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from 'react';
-import { ApplicationContext } from '../../context/ApplicationContext';
-
-export default function Divider(props) {
-
-    const { darkMode } = useContext(ApplicationContext)
-
-    return <span className='divider' id={darkMode ? 'dividerDark':''} />
-}
